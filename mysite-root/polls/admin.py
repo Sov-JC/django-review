38bae018db060dc41b8a52977de2bd46aa14d4a3
Jorge Costafreda <jorge.costafreda@yahoo.com>
2020-03-18 15:40:35 -0400

    L.O Customize the admin change list

--- mysite-root/polls/admin.py
+++ mysite-root/polls/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 
+from .models import Choice
 from .models import Question
 
-# Register your models here.
-admin.site.register(Question)
+
+'''admin.site.register(Question)'''
+
+'''
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text'] '''
+
+'''
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
+	]'''
+
+class ChoiceInline(admin.StackedInline): #can be admin.TabularInline for more compact view
+	model = Choice
+	extra = 3
 
 
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
